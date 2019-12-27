1e62bb41be7bb99cbe3e2f59d71550dfdb6b3b64
KCVO1995 <943514589@qq.com>
2019-12-27 11:20:16 +0800

    可以画点，可以支持手机端

--- main.js
+++ main.js
@@ -14,21 +14,42 @@ function draw() {
     canvas.onmouseup = () => {
         painting = false
     }
+// 判断设备
+    function is_touch_device() {
+        try {
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 
 
-    canvas.onmousemove = (e) => {
-
-        console.log(e.clientX)
-        console.log(e.clientY)
-        if (painting === true) {
-
+    if (is_touch_device()) {
+        canvas.ontouchmove = (x) => {
+            console.log(x.touches[0].clientX)
+            console.log(x.touches[0].clientY)
             ctx.beginPath();
-            ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI, true);
+            ctx.arc(x.touches[0].clientX, x.touches[0].clientY, 10, 0, 2 * Math.PI, true);
             ctx.fill();
             ctx.stroke();
         }
-        else {
+    }
+    else {
+        canvas.onmousemove = (e) => {
+            console.log(e)
+            console.log(e.clientX)
+            console.log(e.clientY)
+            if (painting === true) {
 
+                ctx.beginPath();
+                ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI, true);
+                ctx.fill();
+                ctx.stroke();
+            }
+            else {
+
+            }
         }
     }
 }
