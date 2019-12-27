030576d448d7fcb05c865ae4a32be3cee4452ddd
KCVO1995 <943514589@qq.com>
2019-12-27 10:23:34 +0800

    用canvas画线

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-canvas.onclick = (e) => {
-    console.log(e.clientX)
-    console.log(e.clientY)
-    let div = document.createElement("div")
-    div.style.position = "absolute"
-    div.style.left = e.clientX + 'px'
-    div.style.top = e.clientY + 'px'
-    div.style.width = '10px'
-    div.style.height = '10px'
-    div.style.transform = 'translate(-50%,-50%)'
-    div.style.borderRadius = '50%'
-    div.style.backgroundColor = 'blue'
-    canvas.appendChild(div)
+function draw() {
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.width = document.body.clientWidth
+    canvas.height = document.documentElement.clientHeight
+
+    ctx.fillStyle = "black"
+    ctx.strokeStyle = "none"
+
+    let painting = false
+    canvas.onmousedown = () => {
+        painting = true
+    }
+    canvas.onmouseup = () => {
+        painting = false
+    }
+
+
+    canvas.onmousemove = (e) => {
+
+        console.log(e.clientX)
+        console.log(e.clientY)
+        if (painting === true) {
+
+            ctx.beginPath();
+            ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI, true);
+            ctx.fill();
+            ctx.stroke();
+        }
+        else {
+
+        }
+    }
 }
